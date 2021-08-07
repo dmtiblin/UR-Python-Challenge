a05842790a7588d3e36703661ec845e4450a0cb4
Diane Tiblin <tiblindm@gmail.com>
2021-08-07 19:58:38 -0400

    WIP time period scripted

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,7 +21,29 @@ with open(csvpath, newline= '') as csvfile:
 
 
 # calculate total number of months included in the dataset
+#format the date into datetime
+from datetime import datetime 
 
+working_dates= []
+
+for date in Date:
+    
+    working_dates.append(datetime.strptime(date, '%b-%Y').date())
+
+#find max & min
+latest_date = max(working_dates)
+earliest_date = min(working_dates)
+print(earliest_date)
+print(latest_date)
+
+#calculate # of months # WIP 
+
+totalmonths = len(working_dates)
+print(totalmonths)
+
+totaldays = (latest_date-earliest_date) 
+totalmonths2 = totaldays / 30
+print(totalmonths2)
 
 # calculate net total amount of "Profit/Losses" over the entire period
 
