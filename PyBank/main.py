adf8d3a31a414a4834dbd7ace6e97a1c0d5a4a10
Diane Tiblin <tiblindm@gmail.com>
2021-08-07 18:41:51 -0400

    created python lists for csv values

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,3 +6,38 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 #open and read the csv file
 with open(csvpath, newline= '') as csvfile:
     csvreader = csv.reader(csvfile)
+
+    #skip the header
+    next(csvreader)
+
+    #create variables for list data
+    Date = []
+    PL = [] 
+
+    #add data to python lists
+    for row in csvreader:
+        Date.append(row[0])
+        PL.append(row[1])
+
+
+# calculate total number of months included in the dataset
+
+
+# calculate net total amount of "Profit/Losses" over the entire period
+
+
+
+# Calculate the changes in "Profit/Losses" over the entire period, 
+
+
+# then find the average of those changes
+
+
+# find the greatest increase in profits (date and amount) over the entire period
+
+
+# find the greatest decrease in profits (date and amount) over the entire period
+
+
+
+
