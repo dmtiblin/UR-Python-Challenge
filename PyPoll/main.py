e8f368b3b7ff3db48e19b2fd0b5934891d3773d1
Diane Tiblin <tiblindm@gmail.com>
2021-08-07 21:52:04 -0400

    starter code

---
+++ PyPoll/main.py
@@ -0,0 +1,36 @@
+# import os and create file path
+import os 
+import csv
+csvpath = os.path.join("Resources", "election_data.csv")
+
+#open and read the csv file
+with open(csvpath, newline= '') as csvfile:
+    csvreader = csv.reader(csvfile)
+    #skip the header
+    next(csvreader)
+    #create variables for list data
+    VoterID = []
+    County = []
+    Candidate = []
+    #add data to python lists
+    for row in csvreader:
+        VoterID.append(row[0])
+        County.append(row[1])
+        Candidate.append(row[2])
+
+# calculate total number of votes cast
+
+# generate a complete list of candidates who received votes
+     # UNIQUE
+
+# calculate total number of votes each candidate won
+
+# calculate percentage of votes each candidate won
+
+# calculate the winner of the election based on popular vote.
+
+# print final results
+print("Election Results")
+print("-----------------------")
+
+#export to text file 
