cf030b3c26f5e22665a147a91a302d25c5f8a30d
Diane Tiblin <tiblindm@gmail.com>
2021-08-08 17:25:34 -0400

    almost there

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,23 +22,59 @@ with open(csvpath, newline= '') as csvfile:
 totalvotes = len(Candidate)
 
 # generate a complete list of candidates who received votes
-#brut forecce method:
+#brute force method:
 CandidatesList= []
+
+# calculate total number of votes each candidate won
+VotesKhan = 0
+VotesCorrey = 0
+VotesLi = 0
+VotesOTooley = 0
+
 for x in Candidate:
     if x not in CandidatesList:
         CandidatesList.append(x)
-print(CandidatesList)
-
-# calculate total number of votes each candidate won
-
+    if x == 'Khan':
+        VotesKhan = VotesKhan + 1
+    elif x == 'Correy':
+        VotesCorrey = VotesCorrey + 1
+    elif x == 'Li':
+        VotesLi = VotesLi + 1
+    elif x == "O'Tooley":
+        VotesOTooley = VotesOTooley + 1
 
 # calculate percentage of votes each candidate won
 
+    PercentageKhan = '{:.2%}'.format(VotesKhan / totalvotes)
+    PercentageCorrey = "{:.2%}".format(VotesCorrey / totalvotes)
+    PercentageLi = "{:.2%}".format(VotesLi / totalvotes)
+    PercentageOTooley = "{:.2%}".format(VotesOTooley / totalvotes)
+
+#create dictionary with stats
+CandidateStats = [
+    {'Candidate' :'Khan', 'Votes' : VotesKhan, 'VotesPercent' : PercentageKhan},
+    {'Candidate' : 'Correy', 'Votes' : VotesCorrey, 'VotesPercent' : PercentageCorrey},
+    {'Candidate' : 'Li', 'Votes' : VotesLi, 'VotesPercent' : PercentageLi},
+    {'Candidate' : 'OTootley', 'Votes' : VotesOTooley, 'VotesPercent' : PercentageOTooley}]
+
 # calculate the winner of the election based on popular vote.
+#use list sort method to sort in order of greatest votes then choose the winner as the first palce in the list 
+SortedResults = sorted(CandidateStats, key=lambda k: k['Votes'], reverse = True) 
+Winner = SortedResults[0]
 
 # print final results
 print("Election Results")
 print("-----------------------")
 print("Total Votes: " + str(totalvotes))
 print("-----------------------")
+print(SortedResults[0])
+print(SortedResults[1])
+print(SortedResults[2])
+print(SortedResults[3])
+print("-----------------------")
+print("The Winner is " + str(Winner))
 #export to text file 
+with open("out.txt", "w") as f:
+    f.write("OUTPUT")
+
+  
